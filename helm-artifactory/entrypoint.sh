b8e546b529fed56a9909ceb1dc30b041c55646c8
Martin Weber <martin.weber@spring-media.de>
2019-02-15 07:31:41 +0100

    fmt

--- helm-artifactory/entrypoint.sh
+++ helm-artifactory/entrypoint.sh
@@ -4,9 +4,9 @@ set -o errexit
 set -o pipefail
 
 package() {
-    helm init --client-only
-    helm lint ${CHART}
-    helm package ${CHART}
+  helm init --client-only
+  helm lint ${CHART}
+  helm package ${CHART}
 }
 
 AF_API_USER=$1
@@ -16,20 +16,20 @@ CHART=$4
 
 REPOSITORY="https://${AF_API_USER}:${AF_API_TOKEN}@${AF_URL}/${AF_REPOSITORY}"
 
-if [[ -z $1 ]] ; then
-  echo "No Artifactory API user specified!" && exit 1;
+if [[ -z $1 ]]; then
+  echo "No Artifactory API user specified!" && exit 1
 fi
 
-if [[ -z $2 ]] ; then
-  echo "Artifactory URL parameter needed!" && exit 1;
+if [[ -z $2 ]]; then
+  echo "Artifactory URL parameter needed!" && exit 1
 fi
 
-if [[ -z $3 ]] ; then
-  echo "Helm chart repository not specified!" && exit 1;
+if [[ -z $3 ]]; then
+  echo "Helm chart repository not specified!" && exit 1
 fi
 
-if [[ -z $4 ]] ; then
-  echo "Chart path parameter needed!" && exit 1;
+if [[ -z $4 ]]; then
+  echo "Chart path parameter needed!" && exit 1
 fi
 
-exit 0;
+exit 0
