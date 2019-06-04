efc4be0c168eed087f54ad6c2dc06a8974f65747
Büttner, Jonas <jonas.buettner@spring-media.de>
2019-06-04 15:51:38 +0200

    update createFeature

--- createFeature/entrypoint.sh
+++ createFeature/entrypoint.sh
@@ -1,17 +1,20 @@
 #!/bin/ash
+# shellcheck shell=dash
+################################################################################
+# Description:
+#   Script Github Actions to create a new feature branch automatically
+################################################################################
 set -eu
 set -o pipefail
 
-main() {
-   issuenr=$(jq --raw-output .issue.number "$GITHUB_EVENT_PATH")
-   current_release_branch=$(git branch | grep "release")
-   echo "DEBUG: issue number: '$issuenr'"
-   echo "DEBUG: current release branch: '$current_release_branch'"
+echo "DEBUG: start to create new feature branch!"
 
-   git checkout $current_release_branch
-   git checkout -b feature/$issuenr
-   git push origin feature/$issuenr
-   curl -X POST -H "Accept: application/vnd.github.squirrel-girl-preview" -H"Authorization: token $GITHUB_TOKEN" https://api.github.com/repos/$GITHUB_REPOSITORY/issues/$issuenr/comments -d '{"body": "https://github.com/'$GITHUB_REPOSITORY'/tree/feature/'$issuenr'"}'
-}
+issuenr=$(jq --raw-output .issue.number "$GITHUB_EVENT_PATH")
+current_release_branch=$(git branch | grep "release")
+echo "DEBUG: issue number: '$issuenr'"
+echo "DEBUG: current release branch: '$current_release_branch'"
 
-main
+git checkout $current_release_branch
+git checkout -b feature/$issuenr
+git push origin feature/$issuenr
+curl -X POST -H "Accept: application/vnd.github.squirrel-girl-preview" -H"Authorization: token $GITHUB_TOKEN" https://api.github.com/repos/$GITHUB_REPOSITORY/issues/$issuenr/comments -d '{"body": "https://github.com/'$GITHUB_REPOSITORY'/tree/feature/'$issuenr'"}'
