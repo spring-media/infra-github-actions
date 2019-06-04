84e413fcf3c74f3f0b2e07deadddd0a21c5b4e5a
Büttner, Jonas <jonas.buettner@spring-media.de>
2019-06-04 17:02:19 +0200

    add logging

--- createFeature/entrypoint.sh
+++ createFeature/entrypoint.sh
@@ -15,12 +15,17 @@ getIssueNr() {
 }
 
 echo "DEBUG: start to create new feature branch!"
+
 issue_nr="$(getIssueNr)"
 echo "DEBUG: issue number: $issue_nr"
-current_release_branch="$(getRelease)"
+
+git status
+git branch
+current_release_branch=$(git branch | grep release)
 echo "DEBUG: current release branch: $current_release_branch"
 
 git checkout "$current_release_branch"
 git checkout -b feature/"$issue_nr"
 git push origin feature/"$issue_nr"
+
 curl -X POST -H "Accept: application/vnd.github.squirrel-girl-preview" -H"Authorization: token $GITHUB_TOKEN" https://api.github.com/repos/"$GITHUB_REPOSITORY"/issues/"$issue_nr"/comments -d '{"body": "https://github.com/'"$GITHUB_REPOSITORY"'/tree/feature/'"$issue_nr"'"}'
