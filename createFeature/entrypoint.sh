e0841f00a744938d5f8b05390689d6f632c2d839
Büttner, Jonas <jonas.buettner@spring-media.de>
2019-06-05 10:22:00 +0200

    add logging

--- createFeature/entrypoint.sh
+++ createFeature/entrypoint.sh
@@ -26,4 +26,5 @@ git checkout "$current_release_branch"
 git checkout -b feature/"$issue_nr"
 git push origin feature/"$issue_nr"
 
+echo "DEBUG: post comment on issue with branch path"
 curl -X POST -H "Accept: application/vnd.github.squirrel-girl-preview" -H"Authorization: token $GITHUB_TOKEN" https://api.github.com/repos/"$GITHUB_REPOSITORY"/issues/"$issue_nr"/comments -d '{"body": "https://github.com/'"$GITHUB_REPOSITORY"'/tree/feature/'"$issue_nr"'"}'
